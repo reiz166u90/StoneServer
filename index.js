ff0c854dc4a595f1bf863a1cc094d2f1af855923
reiz166u90 <reired63@gmail.com>
2024-10-22 00:57:44 +0800

    Update1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require("express"); // Corrected the way express is imported
-const mongoose = require("mongoose");
+const express = require("express");
+const mongoose = require('mongoose');
+
 const cors = require("cors");
 const http = require('http');
 
